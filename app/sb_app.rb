a7d3c32b744be8d822a77146470257d64be79cbd
pavanpatil-sacumen <pavan.patil@sacumen.com>
2025-05-06 17:12:35 +0530

    added remaining api edpoint and created postman collection for all

--- app/sb_app.rb
+++ app/sb_app.rb
@@ -38,12 +38,48 @@ class SBApp < Sinatra::Base
     plan = Catalog.instance.find_plan(plan_id)
     logger.info "Provision Request Received: Plan: #{plan_id} - Service Instance ID: #{id}"
     response = Teamserver.provision(id, plan.credentials)
-    if response.code == 201 || ENV['CONTRAST_BACKWARDS_COMPAT']
+    # if response.code == 201 || ENV['CONTRAST_BACKWARDS_COMPAT']
+    if response.code == 201
     	status 201
       {}.to_json
     else
       status response.code
       {:description => 'Unable to provision service instance'}.to_json
     end
+  end
+
+  delete '/v2/service_instances/:instance_id' do |instance_id|
+    content_type :json
+    plan_id = params[:plan_id]
+
+    plan = Catalog.instance.find_plan(plan_id)
+    logger.info "Unprovision Request Received: Plan: #{plan_id} - Service Instance ID: #{instance_id}"
+    Teamserver.unprovision(instance_id, plan.credentials)
+    status 200
+    {}.to_json
+  end
+
+  put '/v2/service_instances/:instance_id/service_bindings/:id' do |instance_id, binding_id|
+  	content_type :json
+
+  	plan_id = params[:plan_id]
+  	logger.info "Bind Request Received - Plan: #{plan_id}"
+    plan = Catalog.instance.find_plan(plan_id)
+    res = Teamserver.bind(instance_id, binding_id, plan.credentials)
+
+    if plan
+      status 200
+      {:credentials => plan.credentials}.to_json
+    else
+      status 404
+      {:msg => 'Service Instance not found'}.to_json
+    end
+  end
+
+  delete '/v2/service_instances/:instance_id/service_bindings/:id' do |instance_id, binding_id|
+    content_type :json
+    logger.info 'Unbind Request Received'
+    status 200
+    {}.to_json
   end
 end
